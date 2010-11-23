34f365fe2009dde089697b7ad62f6cb65be67b92
Chris Eberle <eberle1080@gmail.com>
2010-11-22 22:38:35 -0700

    Added licensing stuff

--- client/test.php
+++ client/test.php
@@ -3,12 +3,15 @@
 $user = 'chris';
 $host = '128.198.58.14';
 $port = 42;
+$dsthost = '10.0.0.8';
+$dstport = 80;
 $timeout = 60;
 
-$server_host = '127.0.0.1';
+$server_host = '10.0.0.1';
 $server_port = 8000;
 
-$request = xmlrpc_encode_request('open', array($user, $host, $port, $timeout));
+//$request = xmlrpc_encode_request('open', array($user, $host, $port, $timeout));
+$request = xmlrpc_encode_request('forward', array($user, $host, $port, $dsthost, $dstport, $timeout));
 
 $context = stream_context_create(array('http' => array(
     'method' => "POST",
