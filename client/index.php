ca4ad035a966d439c971a2b1fd7d3331ead76cc0
Chris Eberle <eberle1080@gmail.com>
2010-12-25 19:17:49 -0700

    Refactoring

--- client/index.php
+++ client/index.php
@@ -5,6 +5,13 @@ define('LDAP_PORT', 389);
 define('LDAP_TOP', 'ou=People,dc=chriseberle,dc=net');
 define('INCLUDES', 1);
 
+//if($_SERVER["HTTPS"] != "on")
+//{
+//    header("HTTP/1.1 301 Moved Permanently");
+//    header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
+//    exit();
+//}
+
 $inactive = 10 * 60; // 10 minutes
 
 session_start();
@@ -34,45 +41,61 @@ function LDAPauth($rdn, $passwd)
     return $ds;
 }
 
-if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != true)
+function showLogin()
 {
-    if(!isset($_POST['luname']) && !isset($_POST['lupass']))
+?>
+<html>
+<head>
+  <title>Login please</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <form method="post">
+  <table id="login">
+
+    <tr>
+      <td>Name:</td>
+      <td><input type="text" name="luname" /></td>
+    </tr>
+
+    <tr>
+      <td>Pass:</td>
+      <td><input type="password" name="lupass" /></td>
+    </tr>
+
+    <tr>
+      <td colspan="2" style="text-align: right;">
+        <input type="submit" value="Log in" />
+      </td>
+    </tr>
+
+  </table>
+  </form>
+</body>
+</html>
+<?php
+}
+
+function processLogin()
+{
+    $uname = "uid=" . trim($_POST['luname']);
+    $upass = trim($_POST['lupass']);
+
+    LDAPconnect();
+    if(!LDAPauth($uname, $upass))
     {
-        echo <<<EOL
-<html><head><title>Login please</title></head><body>
-<form method="post"><table><tr>
-<td>Name:</td>
-<td><input type="text" name="luname" /></td>
-</tr><tr>
-<td>Pass:</td>
-<td><input type="password" name="lupass" /></td>
-</tr><tr>
-<td colspan="2" style="text-align: right;">
-<input type="submit" value="Log in" />
-</td></tr></table></form>
-</body></html>
-EOL;
+        echo "Fail";
     }
     else
     {
-        $uname = "uid=" . trim($_POST['luname']);
-        $upass = trim($_POST['lupass']);
+        $_SESSION['logged_in'] = true;
+        $_SESSION['user'] = trim($_POST['luname']);
 
-        LDAPconnect();
-        if(!LDAPauth($uname, $upass))
-        {
-            echo "Fail";
-        }
-        else
-        {
-            $_SESSION['logged_in'] = true;
-            $_SESSION['user'] = trim($_POST['luname']);
-
-            header("Location: " . $_SERVER['PHP_SELF']);
-        }
+        header("Location: " . $_SERVER['PHP_SELF']);
     }
 }
-else
+
+function showMainPage()
 {
     if(isset($_SESSION['timeout']))
     {
@@ -81,15 +104,15 @@ else
         {
             session_destroy();
             header("Location: " . $_SERVER['PHP_SELF']);
-            die();
+            exit();
         }
     }
 
-    if(isset($_GET['expire']))
+    if(isset($_GET['logout']))
     {
         session_destroy();
         header("Location: " . $_SERVER['PHP_SELF']);
-        die();        
+        exit();
     }
 
     $_SESSION['timeout'] = time();
@@ -97,3 +120,18 @@ else
     require 'ports.php';
 }
 
+if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] != true)
+{
+    if(!isset($_POST['luname']) && !isset($_POST['lupass']))
+    {
+        showLogin();
+    }
+    else
+    {
+        processLogin();
+    }
+}
+else
+{
+    showMainPage();
+}
