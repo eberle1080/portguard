5aafdd77bbf9fa8701d1672ac8d88975dc624c18
Chris Eberle <eberle1080@gmail.com>
2010-12-25 20:49:50 -0700

    Switched to table layout

--- client/ports.php
+++ client/ports.php
@@ -8,13 +8,91 @@ if(!defined('INCLUDES'))
 $host = $_SERVER['REMOTE_ADDR'];
 $user = $_SESSION['user'];
 
-$timeout = 60;
-
 if(!isset($_POST['iport']) && !isset($_POST['fport']) &&
    !isset($_POST['fdsthost']) && !isset($_POST['fdstport']))
 {
+    showPortPage($user, $host);
+}
+elseif(isset($_POST['iport']))
+{
+    $port = (int)$_POST['iport'];
+    $request = xmlrpc_encode_request('open', array($user, $host, $port, $timeout * 60));
+    runOpenRequest($request);
+}
+elseif(isset($_POST['fport']) || isset($_POST['fdsthost']) || isset($_POST['fdstport']))
+{
+    $fport = (int)$_POST['fport'];
+    $dsthost = trim($_POST['fdsthost']);
+    $dstport = (int)$_POST['fdstport'];
+    $request = xmlrpc_encode_request('forward', array($user, $host, $fport, $dsthost, $dstport, $timeout * 60));
+    runOpenRequest($request);
+}
+else
+{
+    die("Unknown error");
+}
+
+function showPortPage($user, $host)
+{
+    global $config;
+
+    ?>
+<html>
+<head>
+  <title>Open ports</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div class="topbar">
+     <div class="topleft">You are: <?php echo "$user@$host"; ?></div>
+    <div class="topright"><a href="?logout">Log out</a></div>
+  </div>
+
+  <h2>Router Ports</h2>
+  <form method="post">
+    <table class="mainTable">
+      <thead>
+        <tr>
+          <th>User</th>
+          <th>Remote IP</th>
+          <th>Router Port</th>
+          <th>Timeout</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Bob</td>
+          <td>1.2.3.4</td>
+          <td>443</td>
+          <td>12/22/2010 8:45 PM</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td><input type="text" readonly="readonly" value="<?php echo $user; ?>" /></td>
+          <td><input type="text" readonly="readonly" value="<?php echo $host; ?>" /></td>
+          <td><input type="text" name="iport" /></td>
+          <td>
+            <select name="itimeout">
+<?php
+    foreach($config["TIMEOUTS"] as $key => $value)
+    {
+        echo "              <option>$key</option>\n";
+    }
+?>
+            </select>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  </form>
+</body>
+</html>
+<?php
+
+/*
     echo <<<EOL
-<html><head><title>Open ports</title></head><body>
+
 <h1>You are: $user@$host</h1>
 <h2>Open port directly to router ($timeout minute timeout)</h2>
 <form method="post"><table><tr>
@@ -43,24 +121,8 @@ if(!isset($_POST['iport']) && !isset($_POST['fport']) &&
 </table></form>
 </body></html>
 EOL;
-}
-elseif(isset($_POST['iport']))
-{
-    $port = (int)$_POST['iport'];
-    $request = xmlrpc_encode_request('open', array($user, $host, $port, $timeout * 60));
-    runOpenRequest($request);
-}
-elseif(isset($_POST['fport']) || isset($_POST['fdsthost']) || isset($_POST['fdstport']))
-{
-    $fport = (int)$_POST['fport'];
-    $dsthost = trim($_POST['fdsthost']);
-    $dstport = (int)$_POST['fdstport'];
-    $request = xmlrpc_encode_request('forward', array($user, $host, $fport, $dsthost, $dstport, $timeout * 60));
-    runOpenRequest($request);
-}
-else
-{
-    die("Unknown error");
+*/
+
 }
 
 function runOpenRequest($request)
